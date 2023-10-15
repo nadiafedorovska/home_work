47bc62af97353561ef07d61e703114d6d01d79de
nadiafedorovska <nadyafedorovskaya16@gmail.com>
2023-10-15 14:54:00 -0500

    last

--- normalize.py
+++ normalize.py
@@ -1,1 +1,17 @@
-print('Hello world!')
+import re 
+
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+
+TRANS = dict()
+
+for cyrillic, latin in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(cyrillic)] = latin
+    TRANS[ord(cyrillic.upper())] = latin.upper()
+
+def normalize(name: str) 
+translate_name = re.sub(r'[^A-Za-z0-9.]', '_', name.translate(TRANS))
+return translate_name
+
+
+    
